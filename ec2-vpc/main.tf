f80400a8302c2cb230a6d941e2ef652067c05083
UdayaKiran442 <gudaya2002@gmail.com>
2024-01-07 16:07:32 +0530

    defined route table association and security groups

--- ec2-vpc/main.tf
+++ ec2-vpc/main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "subnet2" {
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.myvpc
+  vpc_id = aws_vpc.myvpc.id
 }
 
 resource "aws_route_table" "routetable" {
@@ -25,5 +25,48 @@ resource "aws_route_table" "routetable" {
   route {
     gateway_id = aws_internet_gateway.igw.id
     cidr_block = "0.0.0.0/0"
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.routetable.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.routetable.id
+}
+
+resource "aws_security_group" "mysg" {
+  name = "web-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
   }
 }
