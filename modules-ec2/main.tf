fb9e018760a3d8af32ccc2674c6ef99818475d0d
UdayaKiran442 <gudaya2002@gmail.com>
2024-01-08 10:08:11 +0530

    modules in terraform

---
+++ modules-ec2/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = ""
+  instance_type_value = ""
+}
---
+++ modules-ec2/modules/ec2_instance/variables.tf
@@ -0,0 +1,7 @@
+variable "ami_value" {
+  description = "AMI value"
+}
+
+variable "instance_type_value" {
+  description = "Value for instance type"
+}
