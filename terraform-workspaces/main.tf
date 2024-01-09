173c1cd051a71a9e43c1a86144c978f83b957efe
UdayaKiran442 <gudaya2002@gmail.com>
2024-01-09 12:49:28 +0530

    terraform workspaces

---
+++ terraform-workspaces/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+variable "ami" {
+  description = ""
+}
+
+
+variable "instance_type" {
+  description = ""
+  type = map(string)
+
+  default = {
+    "dev" = "t2.micro"
+    "stage" = "t2.medium"
+    "prod" = "t2.xlarge"
+  }
+}
+
+
+
+module "ec2_instance" {
+  source = "./modules/ec2-instance"
+  ami = var.ami
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+}
